2d5ce99444f0d266d9bc00c86ba939bb5b106c72
Szymon Chmielewski <szymon.chmielewski2908@gmail.com>
2023-04-15 20:11:11 +0200

    adding setting button and new activy

--- app/src/main/java/com/example/pazig_projekt/MainActivity.java
+++ app/src/main/java/com/example/pazig_projekt/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.pazig_projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton SettingsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SettingsButton = (ImageButton) findViewById(R.id.SettingsButton);
+
+        SettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadSettings = new Intent (MainActivity.this, SettingsActivity.class);
+                startActivity(intentLoadSettings);
+            }
+        });
     }
 }
