002cd1473dcef9b14fcebbe20d989a11cf78f605
Szymon Chmielewski <szymon.chmielewski2908@gmail.com>
2023-04-15 21:26:02 +0200

    make start and score click acitivity

--- app/src/main/java/com/example/pazig_projekt/MainActivity.java
+++ app/src/main/java/com/example/pazig_projekt/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pazig_projekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -10,12 +11,16 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton SettingsButton;
+    ImageButton StartButton;
+    ImageButton ScoreButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         SettingsButton = (ImageButton) findViewById(R.id.SettingsButton);
+        StartButton = (ImageButton) findViewById(R.id.StartButton);
+        ScoreButton = (ImageButton) findViewById(R.id.ScoreButton);
 
         SettingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +29,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intentLoadSettings);
             }
         });
+
+        StartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadStart = new Intent (MainActivity.this, StartActivity.class);
+                startActivity(intentLoadStart);
+            }
+        });
+
+        ScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadScore = new Intent(MainActivity.this, ScoreActivity.class);
+                startActivity(intentLoadScore);
+            }
+        });
+
     }
 }
