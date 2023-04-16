104ffe355bf74e873810de689a7460fe05e3f3ec
Szymon Chmielewski <szymon.chmielewski2908@gmail.com>
2023-04-16 02:04:20 +0200

    finished StartActivity

--- app/src/main/java/com/example/pazig_projekt/StartActivity.java
+++ app/src/main/java/com/example/pazig_projekt/StartActivity.java
@@ -2,13 +2,53 @@ package com.example.pazig_projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class StartActivity extends AppCompatActivity {
+
+    ImageButton SettingsButton2;
+    ImageButton EasyButton;
+    ImageButton HardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        SettingsButton2 = (ImageButton) findViewById(R.id.SettingsButton2);
+
+        SettingsButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadSettings = new Intent(StartActivity.this, SettingsActivity.class);
+                startActivity(intentLoadSettings);
+            }
+        });
+
+        EasyButton = (ImageButton) findViewById(R.id.EasyButton);
+
+        EasyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadEasy = new Intent(StartActivity.this, EasyActivity.class);
+                startActivity(intentLoadEasy);
+            }
+        });
+
+        HardButton = (ImageButton) findViewById(R.id.HardButton);
+
+        HardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadHard = new Intent(StartActivity.this, HardActivity.class);
+                startActivity(intentLoadHard);
+            }
+        });
+
     }
 }
